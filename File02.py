9997a1fb35941a4af2a82271a434056f7dd52f7c
Liz Uriol <lizjazmin8@gmail.com>
2023-03-27 22:18:08 -0500

    Creando nuevo file

---
+++ File02.py
@@ -0,0 +1,11 @@
+print("Hola Mundo")
+print('Estudiar con "Python Total" es super\ndivertido')
+print('Estudiar signo \\ es una barra invertida')
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+print("Barra Normal: /")
+print('Barra Invertida: \\')
