4bc74f0c339743a469508d9bf12ff72e2fbce248
Liz Uriol <lizjazmin8@gmail.com>
2023-03-27 21:48:29 -0500

    Creando Archivo Python

---
+++ File_Luriol.py
@@ -0,0 +1,4 @@
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
+print("tu nombre es: " + input("Dime tu nombre: ")+ " " + "\ntu apellido es: " + input("Dime tu apellido: "))
+print(input("¿Qué estás estudiando?"))
